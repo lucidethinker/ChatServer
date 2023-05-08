f1f545852dea85ec5028d5b1e822e01b473090c5
Jasim Ihsan <jasimihsan2@gmail.com>
2023-05-08 10:20:48 +0530

    model added

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require('./app')
+const dotenv = require("dotenv");
+const mongoose = require("mongoose"); 
 
+dotenv.config({path:'./config.env',
+})
 
 process.on('uncaughtException', err => {
     console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
@@ -16,6 +20,20 @@ const server = http.createServer(app);
 
 const port =process.env.PORT || 8000;
 
+const DB = process.env.DBURI.replace("<PASSWORD>",process.env.DBPASSWORD);
+
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+
+}).then((con)=>
+{
+    console.log("success")
+}).catch((err)=>{
+    console.log(err);
+});
 
 
 server.listen(port, () => {
