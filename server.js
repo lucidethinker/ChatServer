123d463d07eb909375ab750106cdf58e3d12b887
Jasim Ihsan <jasimihsan2@gmail.com>
2023-05-08 07:22:27 +0530

    docs: :white_check_mark:

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require('./app')
 
 
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+    });
+    
+
+
 const http = require('http');
 
 
@@ -8,8 +16,16 @@ const server = http.createServer(app);
 
 const port =process.env.PORT || 8000;
 
+
+
 server.listen(port, () => {
     console.log(`App running on port ${port} ...`);
     }
 );
 
+process.on("unhandledRejection",(err)=>{
+    console.log(err);
+    server.close(()=>{
+        process.exit(1);
+    })
+})
